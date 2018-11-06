1aee957ada3a5bd72c14fbc5e04b8bcaa35c8f3b
SachaCR <sacha.clercrenaud@frichti.co>
2018-11-06 22:45:55 +0100

    ADD async actions with redux-thunk to load articles list from server

--- src/pages/Articles/index.js
+++ src/pages/Articles/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
 import List from '../../components/List'
 import Form from '../../components/Form'
 import styles from './styles.module.css'
 
-const Articles = () => (
-  <div className='row mt-5'>
-    <div className='col-md-4 offset-md-1'>
-      <h2 className={styles.title} >Articles</h2>
-      <List />
-    </div>
-    <div className="col-md-4 offset-md-1">
-      <h2 className={styles.title} >Add a new article</h2>
-      <Form />
-    </div>
-  </div>
-)
+
+class ConnectedArticles extends Component {
+  render() {
+    return (
+      <div className='row mt-5'>
+        <div className='col-md-4 offset-md-1'>
+          <h2 className={styles.title} >Articles</h2>
+          <List />
+        </div>
+        <div className="col-md-4 offset-md-1">
+          <h2 className={styles.title} >Add a new article</h2>
+          <Form />
+        </div>
+      </div>
+    )
+  }
+}
+
+const Articles = connect()(ConnectedArticles)
 
 export default Articles
